419ba7bde34b8c8e8f2d07d736da267c8c7db11e
ishamagatarapara1222 <ishamagatarapara@gmail.com>
2026-01-08 12:05:58 +0530

    rename files

---
+++ lw1intro.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+main()
+{
+ printf("My name is isha magatarapara.\n");
+ printf("I am 16 year old.\n");
+ printf("I am study in P.P. Savani school.");	
+}
---
+++ lw_4_1_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    int result;
+
+    printf("Enter value of x: ");
+    scanf("%d", &x);
+
+    printf("Enter value of y: ");
+    scanf("%d", &y);
+
+    result = (x + y) * (x + y);
+
+    printf("Result: %d", result);
+    return 0;
+}
