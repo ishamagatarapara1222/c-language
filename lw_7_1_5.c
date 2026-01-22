d278ebf0412788857406f4b81228847f2ac99b87
Isha Magatarapara <ishamagatarapara@gmail.com>
2026-01-21 21:18:04 -0800

    Update lw_7_1_5.c

--- lw_7_1_5.c
+++ lw_7_1_5.c
@@ -1,10 +1,14 @@
+
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int i, j;
 
-    for (i = 1; i <= 5; i++) {
-        for (j = 1; j <= 6 - i; j++) {
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 5; j >= i; j--)
+        {
             printf("%d ", i);
         }
         printf("\n");
